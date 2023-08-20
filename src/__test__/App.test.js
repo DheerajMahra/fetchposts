bc02450a399d1bcfd4d272d95c8d0810d12e4b37
Dheeraj Mahra <dheeraj.mahra@cars24.com>
2023-08-20 21:29:11 +0530

    feat: add tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { screen, render, userEvent, waitForElementToBeRemoved } from '../test/test-utils';
-import App from '../components/App';
+import { screen, render, userEvent, waitForElementToBeRemoved } from '../test/test-utils'
+import App from '../components/App'
+import { act } from 'react-dom/test-utils'
 
-describe("<LoadMore />", () => {
-  test("Button text is `Loading...` on initial load", () => {
-    render(<App />);
-    expect(1).toBe(1)
-  });
-});
+test("Initial posts are loaded on app start", async () => {
+  render(<App />)
+
+  await waitForElementToBeRemoved(() => screen.queryByRole('button', {name: /loading/i}))
+
+  const items = await screen.findAllByRole("listitem")
+
+  expect(items).toHaveLength(10)
+  expect(screen.queryByRole('button', {name: /loading/i})).not.toBeInTheDocument()
+  expect(screen.getByRole('button', {name: /load more/i})).toBeInTheDocument()
+})
+
+test("Load More button fetches more posts", async () => {
+  render(<App />)
+
+  const loadMoreBtn = await screen.findByRole('button', {name: /load more/i});
+
+  await act(async () => await userEvent.click(loadMoreBtn))
+  await waitForElementToBeRemoved(() => screen.queryByRole('button', {name: /loading/i}))
+
+  const items = await screen.findAllByRole("listitem")
+
+  expect(items).toHaveLength(20)
+  expect(screen.getByRole('button', {name: /load more/i})).toBeInTheDocument()
+})
+
+test("Button show `No more posts` after loading the posts 9 times", async () => {
+  render(<App />)
+
+  const loadMoreBtn = await screen.findByRole('button', {name: /load more/i});
+
+  for(let i=0; i<9; i++) {
+    await act(async () => await userEvent.click(loadMoreBtn))
+  }
+
+  expect(screen.getByRole('button', {name: /no more posts/i})).toBeInTheDocument()
+})
